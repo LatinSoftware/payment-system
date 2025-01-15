6397e3be30799b3d5e99fb8dc60dcc086464ece5
Caonabo Mena <cmena7512@gmail.com>
2025-01-15 10:12:08 -0400

    Refactor Product and ITable interfaces to improve type definitions and add RouteParams interface

--- app/type.d.ts
+++ app/type.d.ts
@@ -1,14 +1,21 @@
 interface Product {
+  id: string;
+  name: string;
+  href: string | null;
+  price: {
     id: string;
-    name: string;
-    href: string | null;
-    price: {
-      id: string;
-      amount: number;
-    };
-    imageSrc: string;
-    imageAlt: string;
-  }
+    amount: number;
+  };
+  imageSrc: string;
+  imageAlt: string;
+}
+interface ITable {
+  token: string;
+  name: string;
+}
 
-
-  
+interface RouteParams {
+  params: {
+    token: string;
+  };
+}
