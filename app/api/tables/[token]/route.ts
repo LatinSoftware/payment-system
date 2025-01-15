5e83d60bbd84b1d38e10717b89b2915717d0d6a2
Caonabo Mena <cmena7512@gmail.com>
2025-01-15 10:11:44 -0400

    Add API route for table retrieval with token validation

---
+++ app/api/tables/[token]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const tables: ITable[] = [
+    {
+        token: "adad9dad-e7fc-43f0-8c8d-415438dc18a1",
+        name: "Mesa 1",
+    },
+    {
+        token: "adad9dad-e7fc-43f0-8c8d-415438dc18a2",
+        name: "Mesa 2",
+    },
+];
+
+export async function GET(request: NextRequest, {params}: RouteParams):Promise<NextResponse>{
+    console.log("creating cookie");
+    const { token } = params;
+    if(!token) 
+        return NextResponse.json({error: "Token is required"}, {status: 400});
+
+    const table = tables.find((item) => item.token === token);
+    
+    if(!table) 
+        return NextResponse.json({error: "Table not found"}, {status: 404});
+
+    const response = NextResponse.redirect(new URL(`/`, request.url));
+    response.cookies.set('tableToken', table.token, { httpOnly: true, secure: true, sameSite: 'strict', maxAge: 60 * 60 * 24 });
+    response.cookies.set('tableName', table.name, { httpOnly: true, secure: true, sameSite: 'strict', maxAge: 60 * 60 * 24 });
+
+    return response;
+
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+    return NextResponse.json(
+      { error: 'Method not allowed' },
+      { status: 405, headers: { Allow: 'GET' } }
+    );
+  }
